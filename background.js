2473fbce0237c8824538346bb5217230be5307f7
Jonathan Eom <jonnyeom@gmail.com>
2020-05-11 22:52:48 -0400

    Load config and apply redirect to configured version

--- background.js
+++ background.js
@@ -1,23 +1,36 @@
-const oldReddit = "https://old.reddit.com";
-
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    // Exclude poll pages
-    if (details.url.match(/^https?:\/\/(www\.)*reddit.com\/poll/)) {
+    let match = details.url.match(/^https?:\/\/[\S\s]*([4-9].[0-9].['x','X'])$/);
+    if (!match) {
       return;
     }
 
-    return {
-      redirectUrl:
-        oldReddit + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]
-    };
+    const current_version = match[1];
+    const drupal_version = current_version.charAt(0);
+    const key = 'drupal' + drupal_version + 'Version';
+
+    // If a cached version does not exist, get it from chrome storage.
+    if (!localStorage[key]) {
+      chrome.storage.sync.get([key], function(result) {
+        if (result[key]) {
+          localStorage[key] = result[key];
+        }
+      });
+      return;
+    }
+
+    // Get cached version.
+    let configured_version = localStorage[key];
+
+    if (configured_version !== current_version) {
+      let original_url = match[0];
+      return {redirectUrl: original_url.substring(0, original_url.length - 5) + configured_version};
+    }
   },
   {
     urls: [
-      "*://reddit.com/*",
-      "*://www.reddit.com/*",
-      "*://np.reddit.com/*",
-      "*://new.reddit.com/*",
+      "*://api.drupal.org/api/drupal/*.x",
+      "*://www.api.drupal.org/api/drupal/*.x",
     ],
     types: [
       "main_frame",
